2493ad134fd9ad0fe2a17fa2c184ee4e06d7d1a0
zhenghongyu <1193073442@qq.com>
2022-05-11 17:02:34 +0800

    第一次提交

---
+++ src/main/java/com/learn/NaviTest.java
@@ -0,0 +1,12 @@
+package com.learn;
+
+/**
+ * @author zhy
+ * @create 2022-05-11 16:59
+ */
+public class NaviTest {
+
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
