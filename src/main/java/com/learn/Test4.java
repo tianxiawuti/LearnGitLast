8eb94f5a463c2c6c3e9d7e674cdfbbf091eeea13
tianxiawuti <47663255+tianxiawuti@users.noreply.github.com>
2022-05-11 19:08:10 +0800

    Create Test4.java

---
+++ src/main/java/com/learn/Test4.java
@@ -0,0 +1,15 @@
+package com.learn;
+
+/**
+ * @author zhy
+ * @create 2022-05-11 17:12
+ */
+public class Test4 {
+
+    public static void main(String[] args) {
+        System.out.println("test2");
+        System.out.println("upd");
+        System.out.println("远程upd");
+        System.out.println("远程再次upd");
+    }
+}
