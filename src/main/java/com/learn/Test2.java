88f3d6f5ebd68fb2c52c04191c846f9e979d602a
zhenghongyu <1193073442@qq.com>
2022-05-11 17:12:46 +0800

    第2次提交

---
+++ src/main/java/com/learn/Test2.java
@@ -0,0 +1,12 @@
+package com.learn;
+
+/**
+ * @author zhy
+ * @create 2022-05-11 17:12
+ */
+public class Test2 {
+
+    public static void main(String[] args) {
+        System.out.println("test2");
+    }
+}
