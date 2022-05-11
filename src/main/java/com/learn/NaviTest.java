08dd156d65122b86e7407680f6e7bdaa29e38e35
zhenghongyu <1193073442@qq.com>
2022-05-11 18:53:16 +0800

    本地第7次提交

--- src/main/java/com/learn/NaviTest.java
+++ src/main/java/com/learn/NaviTest.java
@@ -9,6 +9,6 @@ public class NaviTest {
     public static void main(String[] args) {
         System.out.println("hi");
         System.out.println("第一次解决冲突");
-        System.out.println("本地重新upd");
+        System.out.println("本地重新");
     }
 }
